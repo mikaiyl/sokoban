7220134cb74cbb3f3f06e3c007881b07f8a39604
Mikaiyl <mikaiyl.davis@icloud.com>
2018-05-17 09:48:42 -0400

    done with level two

--- scripts/main.js
+++ scripts/main.js
@@ -14,20 +14,20 @@
 //                     [ 'W',' ',' ',' ',' ',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ','W',' ',' ',' ','W' ],
 //                     [ 'W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W' ] ];
 
-// const mazeArray =   [ [ ' ',' ',' ',' ','W','W','W','W','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
-//                       [ ' ',' ',' ',' ','W',' ',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
-//                       [ ' ',' ',' ',' ','W','B',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
-//                       [ ' ',' ','W','W','W',' ',' ','B','W','W',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
-//                       [ ' ',' ','W',' ',' ','B',' ','B',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
-//                       [ 'W','W','W',' ','W',' ','W','W',' ','W',' ',' ',' ','W','W','W','W','W','W' ],
-//                       [ 'W',' ',' ',' ','W',' ','W','W',' ','W','W','W','W','W',' ',' ','O','O','W' ],
-//                       [ 'W',' ','B',' ',' ','B',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','O','O','W' ],
-//                       [ 'W','W','W','W','W',' ','W','W','W',' ','W','C','W','W',' ',' ','O','O','W' ],
-//                       [ ' ',' ',' ',' ','W',' ',' ',' ',' ',' ','W','W','W','W','W','W','W','W','W' ],
-//                       [ ' ',' ',' ',' ','W','W','W','W','W','W','W',' ',' ',' ',' ',' ',' ',' ',' ' ] ]
+const mazeArrayTwo = [ [ ' ',' ',' ',' ','W','W','W','W','W',' ',' ',' ',' ',' ','LEVEL',' ',' ',' ',' ' ],
+                      [ ' ',' ',' ',' ','W',' ',' ',' ','W',' ',' ',' ',' ',' ','TWO',' ',' ',' ',' ' ],
+                      [ ' ',' ',' ',' ','W','B',' ',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
+                      [ ' ',' ','W','W','W',' ',' ','B','W','W',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
+                      [ ' ',' ','W',' ',' ','B',' ','B',' ','W',' ',' ',' ',' ',' ',' ',' ',' ',' ' ],
+                      [ 'W','W','W',' ','W',' ','W','W',' ','W',' ',' ',' ','W','W','W','W','W','W' ],
+                      [ 'W',' ',' ',' ','W',' ','W','W',' ','W','W','W','W','W',' ',' ','O','O','W' ],
+                      [ 'W',' ','B',' ',' ','B',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','O','O','W' ],
+                      [ 'W','W','W','W','W',' ','W','W','W',' ','W','C','W','W',' ',' ','O','O','W' ],
+                      [ ' ',' ',' ',' ','W',' ',' ',' ',' ',' ','W','W','W','W','W','W','W','W','W' ],
+                      [ ' ',' ',' ',' ','W','W','W','W','W','W','W',' ',' ',' ',' ',' ',' ',' ',' ' ] ]
 
-const mazeArray =   [ [ ' ', ' ', 'W', 'W', 'W', 'W', 'W', ' ' ],
-                      [ 'W', 'W', 'W', ' ', ' ', ' ', 'W', ' ' ],
+const mazeArrayOne =   [ [ ' ', ' ', 'W', 'W', 'W', 'W', 'W', 'LEVEL' ],
+                      [ 'W', 'W', 'W', ' ', ' ', ' ', 'W', 'ONE' ],
                       [ 'W', 'O', 'C', 'B', ' ', ' ', 'W', ' ' ],
                       [ 'W', 'W', 'W', ' ', 'B', 'O', 'W', ' ' ],
                       [ 'W', 'O', 'W', 'W', 'B', ' ', 'W', ' ' ],
@@ -39,8 +39,9 @@ const mazeArray =   [ [ ' ', ' ', 'W', 'W', 'W', 'W', 'W', ' ' ],
 // CHANGE WITH MAZEARRAY
 // char column/row 0,9 11,8 2,2
 const state = {
-    charCol: 2,
-    charRow: 2,
+    charCol: 0,
+    charRow: 0,
+    mazeArray: mazeArrayOne,
     maze: document.getElementById("maze"),
     DOMCells: maze.getElementsByClassName("cell"),
     charachter: document.createElement("div"),
@@ -55,28 +56,44 @@ const state = {
 
 state.charachter.id = "running-man";
 
+state.setCharPos = () => {
+    for( let row in state.mazeArray ) {
+        for( let col in state.mazeArray[row] ) {
+            if ( state.mazeArray[row][col] == "C" ) {
+                debugger
+                state.charCol = parseInt(col);
+                state.charRow = parseInt(row);
+            }
+        }
+    }
+}
+
 /* Create DOM maze from maze array */
 const printMazeToDOM = () => {
-    for ( let mazeArrayRowIndex in mazeArray ) {
+    state.setCharPos();
+    for ( let mazeArrayRowIndex in state.mazeArray ) {
         let DOMRow = document.createElement("div");
         DOMRow.id = "r" + mazeArrayRowIndex;
         DOMRow.classList.add("row");
-        for ( let mazeArrayColumnIndex in mazeArray[mazeArrayRowIndex] ){ // create elements for flexbox
+        for ( let mazeArrayColumnIndex in state.mazeArray[mazeArrayRowIndex] ){ // create elements for flexbox
             let rowIndex = parseInt(mazeArrayRowIndex);
             let columnIndex = parseInt(mazeArrayColumnIndex);
             let DOMcell = document.createElement("div");
             DOMcell.classList.add("cell");
             DOMcell.classList.add("r" + mazeArrayRowIndex);
             DOMcell.classList.add("c" + mazeArrayColumnIndex);
-            if ( mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "W" ) {
+            if ( state.mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "W" ) {
                 DOMcell.dataset.wall = true;
-            } else if ( mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "B" ) {
+            } else if ( state.mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "B" ) {
                 DOMcell.dataset.crate = true;
-            } else if ( mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "O" ) {
+            } else if ( state.mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "O" ) {
                 DOMcell.dataset.point = true;
-            } else if ( mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "X" ) {
+            } else if ( state.mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "X" ) {
                 DOMcell.dataset.point = true;
                 DOMcell.dataset.crate = true;
+            } else if ( state.mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex].length > 1 ) {
+                let letter = state.mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex];
+                DOMcell.textContent = letter;
             } DOMRow.appendChild(DOMcell);
         } state.maze.appendChild(DOMRow);
     } state.maze.querySelector("div.cell.r" + state.charRow + ".c" + state.charCol).appendChild(state.charachter);
@@ -86,12 +103,12 @@ const printMazeToDOM = () => {
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     let row = state.charRow, col = state.charCol;
-    let element = state.maze.querySelector("div.cell.r" + (row) + ".c" + (col));
+    let element = state.maze.querySelector("div.cell.r" + (parseInt(row)) + ".c" + (parseInt(col)));
 
-    if ( keyName == "ArrowDown" &&  mazeArray[row+state.offByOne][col] !== "W" && row < state.dimensions.height - state.offByOne  && !state.isMoving) {
-        let targetElement = state.maze.querySelector("div.cell.r" + (row+1) + ".c" + (col));
+    if ( keyName == "ArrowDown" &&  state.mazeArray[row+state.offByOne][col] !== "W" && row < state.dimensions.height - state.offByOne  && !state.isMoving) {
+        let targetElement = state.maze.querySelector("div.cell.r" + (parseInt(row)+1) + ".c" + (parseInt(col)));
         if ( targetElement.dataset.crate == "true" ) {
-            let nextElement = state.maze.querySelector("div.cell.r" + (row+2) + ".c" + (col));
+            let nextElement = state.maze.querySelector("div.cell.r" + (parseInt(row)+2) + ".c" + (parseInt(col)));
             // move box or not
             if ( nextElement.dataset.crate == "true" || nextElement.dataset.wall == "true" ) {
             } else {
@@ -110,10 +127,10 @@ document.addEventListener('keydown', (event) => {
                 state.charachter.classList.add("animateS");
                 setTimeout(() => unAnimate( "S", moveMan()), state.waitTime);
         }
-    } else if ( keyName == "ArrowUp" &&  mazeArray[row-state.offByOne][col] !== "W" && row > 0  && !state.isMoving ) {
-        let targetElement = state.maze.querySelector("div.cell.r" + (row-1) + ".c" + (col));
+    } else if ( keyName == "ArrowUp" &&  state.mazeArray[row-state.offByOne][col] !== "W" && row > 0  && !state.isMoving ) {
+        let targetElement = state.maze.querySelector("div.cell.r" + (parseInt(row)-1) + ".c" + (parseInt(col)));
         if ( targetElement.dataset.crate == "true" ) {
-            let nextElement = state.maze.querySelector("div.cell.r" + (row-2) + ".c" + (col));
+            let nextElement = state.maze.querySelector("div.cell.r" + (parseInt(row)-2) + ".c" + (parseInt(col)));
             // move box or not
             if ( nextElement.dataset.crate == "true" || nextElement.dataset.wall == "true" ) {
         } else {
@@ -132,10 +149,10 @@ document.addEventListener('keydown', (event) => {
             state.charachter.classList.add("animateN");
             setTimeout(() => unAnimate( "N", moveMan()), state.waitTime);
         }
-    } else if ( keyName == "ArrowLeft" &&  mazeArray[row][col-state.offByOne] !== "W" && col > 0  && !state.isMoving ) {
-        let targetElement = state.maze.querySelector("div.cell.r" + (row) + ".c" + (col-1));
+    } else if ( keyName == "ArrowLeft" &&  state.mazeArray[row][col-state.offByOne] !== "W" && col > 0  && !state.isMoving ) {
+        let targetElement = state.maze.querySelector("div.cell.r" + (parseInt(row)) + ".c" + (parseInt(col)-1));
         if ( targetElement.dataset.crate == "true" ) {
-            let nextElement = state.maze.querySelector("div.cell.r" + (row) + ".c" + (col-2));
+            let nextElement = state.maze.querySelector("div.cell.r" + (parseInt(row)) + ".c" + (parseInt(col)-2));
             // move box or not
             if ( nextElement.dataset.crate == "true" || nextElement.dataset.wall == "true" ) {
             } else {
@@ -154,10 +171,10 @@ document.addEventListener('keydown', (event) => {
             state.charachter.classList.add("animateW");
             setTimeout(() => unAnimate( "W", moveMan()), state.waitTime);
         }
-    } else if ( keyName == "ArrowRight" &&  mazeArray[row][col+state.offByOne] !== "W" && col < state.dimensions.width - state.offByOne  && !state.isMoving ) {
-        let targetElement = state.maze.querySelector("div.cell.r" + (row) + ".c" + (col+1));
+    } else if ( keyName == "ArrowRight" &&  state.mazeArray[row][col+state.offByOne] !== "W" && col < state.dimensions.width - state.offByOne  && !state.isMoving ) {
+        let targetElement = state.maze.querySelector("div.cell.r" + (parseInt(row)) + ".c" + (parseInt(col)+1));
         if ( targetElement.dataset.crate == "true" ) {
-            let nextElement = state.maze.querySelector("div.cell.r" + (row) + ".c" + (col+2));
+            let nextElement = state.maze.querySelector("div.cell.r" + (parseInt(row)) + ".c" + (parseInt(col)+2));
             // move box or not
             if ( nextElement.dataset.crate == "true" || nextElement.dataset.wall == "true" ) {
             } else {
@@ -197,10 +214,14 @@ document.addEventListener('keyup', (event) => {
 /* do the win thing */
 const win = () => {
     alert("You won!");
+    state.mazeArray = mazeArrayTwo;
+    state.maze.innerHTML = "";
+    printMazeToDOM();
 }
 
 /* move charachter */
 const moveMan = () => {
+    debugger;
     state.maze.querySelector("div.cell.r" + state.charRow + ".c" + state.charCol).appendChild(state.charachter);
 }
 
